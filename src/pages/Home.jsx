599639587f60fa1e4c3290b2dcc6d63034d5d1db
Ganesh <koreganesh92@gmail.com>
2026-01-22 22:29:57 +0530

    Built home page with table layout to display the data and api integration

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,49 @@
-import React, { useEffect } from 'react'
-import { getBooks } from '../services/bookService';
+import { useEffect, useState } from "react";
+import { getBooks } from "../services/bookService";
+import { Link } from "react-router-dom";
 
 const Home = () => {
+  const [books, setBooks] = useState([]);
 
   useEffect(() => {
-    getBooks().then((res) => console.log(res.data));
+    getBooks().then((res) => setBooks(res.data));
   }, []);
 
   return (
-    <div>book detail</div>
-  )
-}
+    <div style={{ padding: "20px", maxHeight: "100vh", overflowY: "auto" }}>
+      <h2>Book Inventory</h2>
 
-export default Home
+      <Link to="/add">
+        <button>Add Book</button>
+      </Link>
+
+      <table border="1" width="100%" cellPadding="10" style={{ marginTop: "15px" }}>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Publisher</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {books.map((book) => (
+            <tr key={book.id}>
+              <td>{book.title}</td>
+              <td>{book.author}</td>
+              <td>{book.publisher}</td>
+              <td>
+                <Link to={`/book/${book.id}`}>View</Link>{" | "}
+                <Link to={`/edit/${book.id}`}>Edit</Link>{" | "}
+                <button>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Home;
