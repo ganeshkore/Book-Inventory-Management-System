a015fdf16176039cb6e2583527d4776f6e71bf60
Ganesh <koreganesh92@gmail.com>
2026-01-22 22:17:53 +0530

    created json server api setup  and created axios service layer for api calls

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { getBooks } from '../services/bookService';
 
 const Home = () => {
+
+  useEffect(() => {
+    getBooks().then((res) => console.log(res.data));
+  }, []);
+
   return (
     <div>book detail</div>
   )
