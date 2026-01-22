eeccc6ebaa94b34511de846b8f5a516acbbd3dcd
Ganesh <koreganesh92@gmail.com>
2026-01-22 21:59:19 +0530

    created project structure and created routes for different views like home addbook , editbook

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from 'react'
 import './App.css'
+import AddBook from './pages/AddBook'
+import BookDetails from './pages/BookDetails'
+import EditBook from './pages/EditBook'
+import Home from './pages/Home'
+import { BrowserRouter , Routes , Route } from 'react-router-dom'
 
 function App() {
 
   return (
-    <>
-     <h1>King Ganesh </h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add" element={<AddBook />} />
+        <Route path="/edit/:id" element={<EditBook />} />
+        <Route path="/book/:id" element={<BookDetails />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>book detail</div>
+  )
+}
+
+export default Home
