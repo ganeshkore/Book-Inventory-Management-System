78ee26d0edd21405d594681488cdd48cc8e678e5
Ganesh <koreganesh92@gmail.com>
2026-01-23 12:11:47 +0530

    added isons for action buttons with title and enhanced ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { getBooks, deleteBook } from "../services/bookService";
 import { Link } from "react-router-dom";
+import { Eye, Pencil, Trash2, Plus } from "lucide-react";
+
 const Home = () => {
   const [books, setBooks] = useState([]);
 
@@ -27,9 +29,10 @@ const Home = () => {
 
         <Link
           to="/add"
-          className="mt-3 md:mt-0 px-5 py-2 rounded-lg bg-indigo-600 hover:bg-indigo-500 transition"
+          className="mt-3 md:mt-0 px-5 py-2 rounded-lg bg-indigo-600 hover:bg-indigo-500 transition flex items-center gap-2"
         >
-          + Add Book
+          <Plus size={18} />
+          Add Book
         </Link>
       </div>
 
@@ -52,27 +55,30 @@ const Home = () => {
               >
                 <td className="p-4">{book.title}</td>
                 <td className="p-4">{book.author}</td>
-                <td className="p-4 hidden md:table-cell">
-                  {book.publisher}
-                </td>
-                <td className="p-4 flex gap-3 justify-center">
+                <td className="p-4 hidden md:table-cell">{book.publisher}</td>
+                <td className="p-4 flex gap-4 justify-center">
                   <Link
                     to={`/book/${book.id}`}
-                    className="text-indigo-400 hover:underline"
+                    className="text-indigo-400 hover:text-indigo-300 transition"
+                    title="View"
                   >
-                    View
+                    <Eye size={18} />
                   </Link>
+
                   <Link
                     to={`/edit/${book.id}`}
-                    className="text-yellow-400 hover:underline"
+                    className="text-yellow-400 hover:text-yellow-300 transition"
+                    title="Edit"
                   >
-                    Edit
+                    <Pencil size={18} />
                   </Link>
+
                   <button
                     onClick={() => handleDelete(book.id)}
-                    className="text-red-400 hover:underline"
+                    className="text-red-400 hover:text-red-300 transition"
+                    title="Delete"
                   >
-                    Delete
+                    <Trash2 size={18} />
                   </button>
                 </td>
               </tr>
